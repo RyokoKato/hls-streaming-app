6bfe54d7f244f1281137ba84b195ec0cc4541da0
RyokoKato <amedama.1017@gmail.com>
2026-04-14 15:40:07 +0900

    add: frontサーバのドメインもコンテナビルド時にconfig.jsを書き換える

--- front/entrypoint.sh
+++ front/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 ## Replace API Domain
-echo "Replace API Domain in index.html"
+echo "Replace API Domain in js/config.js"
 sed -i "s|__API_DOMAIN__|${API_DOMAIN_NAME}|g" /usr/share/nginx/data/www/js/config.js
+
+## Replace Front Domain
+echo "Replace Front Domain in js/config.js"
+sed -i "s|__FRONT_DOMAIN__|${FRONT_DOMAIN_NAME}|g" /usr/share/nginx/data/www/js/config.js
 
 # 最後にNginxを起動
 exec nginx -g 'daemon off;'
